f4296b8012144dff953afb8e3e9f30a308372388
geena-keem <geena.gamm@gmail.com>
2021-05-25 19:09:12 +0900

    add

---
+++ dream-coding/1/main.js
@@ -0,0 +1,10 @@
+// Whole-script strict mode syntax (엄격 모드)
+// JavaScript is very flexible
+// flexible === dangerous
+// added ECMAScript 5
+'use strict';
+
+console.log('hello word!');
+
+let a;
+a = 6;
