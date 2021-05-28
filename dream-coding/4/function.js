7864a2d0d8e102fcbd535f73b997a548224cb022
geena-keem <geena.gamm@gmail.com>
2021-05-28 20:23:35 +0900

    add quiz-!

--- dream-coding/4/function.js
+++ dream-coding/4/function.js
@@ -188,3 +188,20 @@ const simpleMultiply = (a, b) => {
 // Fun quiz time
 // function calculate(command, a, b)
 // command: add, substract, divide, multiply, remainder
+function calculate(command, a, b) {
+  switch (command) {
+    case 'add':
+      return a + b;
+    case 'substract':
+      return a - b;
+    case 'divide':
+      return a / b;
+    case 'multiply':
+      return a * b;
+    case 'remainder':
+      return a % b;
+    default:
+      break;
+  }
+}
+console.log(calculate('add', 1, 2));
