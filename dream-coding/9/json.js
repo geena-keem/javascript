c29c2aae9fe8245fd1cf6b29789c7334d1d80fee
geena-keem <geena.gamm@gmail.com>
2021-06-08 12:44:54 +0900

    add json

---
+++ dream-coding/9/json.js
@@ -0,0 +1,8 @@
+// JSON
+// - simplest data interchange format
+// - lightweight text-based structure
+// - easy to read
+// - key-value pairs
+// - used for serialization and transmission of data between the network the network connection
+//   (네트워크 간의 데이터 직렬화 및 전송에 사용)
+// - independent programming language and platform******
