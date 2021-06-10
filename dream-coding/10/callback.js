cb27c128b660ee663a92e6712d44bb793981c586
geena-keem <geena.gamm@gmail.com>
2021-06-10 22:16:23 +0900

    add callback

---
+++ dream-coding/10/callback.js
@@ -0,0 +1,8 @@
+'use strict';
+
+// JavaScript is synchronous.
+// Execute the code block by orger after hoisting.
+// hoisting: var, function declaration
+console.log('1');
+setTimeout(() => console.log('2'), 1000);
+console.log('3');
